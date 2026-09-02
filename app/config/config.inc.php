d59e6a5489662320e687cdf3d9c618cb3efe516e
Anil Kapoor <anilchd.kapoor93@gmail.com>
2026-09-02 16:34:04 +0530

    initial commit

---
+++ app/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("PJ_HOST")) define("PJ_HOST", "localhost");
+if (!defined("PJ_USER")) define("PJ_USER", "root");
+if (!defined("PJ_PASS")) define("PJ_PASS", "");
+if (!defined("PJ_DB")) define("PJ_DB", "FoodDeliveryDev-v4.1");
+if (!defined("PJ_PREFIX")) define("PJ_PREFIX", "");
+if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/FoodDeliveryDev-v4.1/");
+if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "/Applications/XAMPP/xamppfiles/htdocs/FoodDeliveryDev-v4.1/");
+if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://localhost/FoodDeliveryDev-v4.1/");
+if (!defined("PJ_SALT")) define("PJ_SALT", "AUAJ1U4X");
+if (!defined("PJ_INSTALLATION")) define("PJ_INSTALLATION", "9d2f24a4059f0811f96269d2f95b6ca7");
+?>
